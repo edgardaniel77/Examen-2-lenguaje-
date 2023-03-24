61b6b98b920f65c7ba38bb57c3cb160ce3e4686f
edgardaniel77 <edgardaguilera3@gmail.com>
2023-03-23 20:49:44 -0600

    primer commit
    examen 2

---
+++ Entidades/Login.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace Entidades
+{
+    public class Login
+    {
+        public string CodigoUsuario { get; set; }
+        public string Contraseña { get; set; }
+
+        public Login()
+        {
+
+        }
+
+        public Login(string codigoUsuario, string contraseña)
+        {
+            CodigoUsuario = codigoUsuario;
+            Contraseña = contraseña;
+        }
+       
+    }
+
+}
